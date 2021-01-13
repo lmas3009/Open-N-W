2518669aef022bdb14e3fe9ef729aacf3c19e844
unknown <30lmas09@gmail.com>
2021-01-13 19:49:17 +0530

    new edit

--- news/urls.py
+++ news/urls.py
@@ -8,9 +8,18 @@
 # ]
 
 
-from .views import NewsViewSet
-from rest_framework.routers import DefaultRouter
+# from .views import NewsViewSet
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'', NewsViewSet, basename='user')
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r'', NewsViewSet, basename='user')
+# urlpatterns = router.urls
+
+
+from django.urls import path
+from .views import Latest_News,Indexed_id,Latest_Country_News
+
+urlpatterns=[
+    path('API=<str:api>&Country=<str:name>/',Latest_Country_News),
+    path('API=<str:api>&Countrycode=<str:name>&Id=<int:id>/',Indexed_id)
+]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,59 @@
+# from django.shortcuts import render
+# from rest_framework import viewsets
+# from .models import News
+# from .serializer import NewsSerializer
+
+
+# class NewsViewSet(viewsets.ModelViewSet):
+#     queryset = News.objects.all()
+#     serializer_class = NewsSerializer
+
 from django.shortcuts import render
-from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
 from .models import News
 from .serializer import NewsSerializer
+from django.http import HttpResponse,HttpRequest,JsonResponse
+import json
+import urllib.request
 
 
-class NewsViewSet(viewsets.ModelViewSet):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
+
+# Create your views here.e2b1a670aa6c406a821291c1350e7b90
+
+
+@csrf_exempt
+def Latest_News(request,api):
+
+    if(request.method=='GET'):
+        url = ('http://newsapi.org/v2/top-headlines?'
+            'country=us&'
+            'apiKey='+api)
+        response = urllib.request.urlopen(url).read().decode('UTF-8')
+        data = json.loads(response)
+        dat = data["articles"]
+        return JsonResponse(dat,safe=False)
+
+@csrf_exempt
+def Latest_Country_News(request,name,api):
+    print(name,api)
+    if(request.method=='GET'):
+        url = ('http://newsapi.org/v2/top-headlines?'
+            'country='+name+'&'
+            'apiKey='+api)
+        print(url)
+        response = urllib.request.urlopen(url).read().decode('UTF-8')
+        data = json.loads(response)
+        dat = data["articles"]
+        return JsonResponse(dat,safe=False)
+    
+@csrf_exempt
+def Indexed_id(request,id,api,name):
+    print(api,name,id)
+    if(request.method=='GET'):
+        url = ('http://newsapi.org/v2/top-headlines?'
+            'country='+name+'&'
+            'apiKey='+api)
+        response = urllib.request.urlopen(url).read().decode('UTF-8')
+        data = json.loads(response)
+        dat = data["articles"]
+        return JsonResponse(dat[id],safe=False)
